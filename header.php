f223efeef51501ad179d940f2084c6240d2f479e
Martin <emartin@cmaisonneuve.qc.ca>
2025-02-19 17:29:48 -0500

    s5c1 début du footer

--- header.php
+++ header.php
@@ -20,40 +20,7 @@
                     'container' => 'nav',
                     'container_class' => 'entete__menu'
                 )); ?>
-                <nav class="entete__menu">
-                    <ul class="menu">
-                        <li class="menu__li">
-                            <a href="#">Aventure</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Culturel</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Zen</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Sportive</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Tourtereau</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Aventure</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Culturel</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Zen</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Sportive</a>
-                        </li>
-                        <li class="menu__li">
-                            <a href="#">Tourtereau</a>
-                        </li>
-                    </ul>
-                </nav>
+                
                 <form class="recherche">
                     <input type="search" placeholder="Rechercher" class="recherche__input">
                     <img class="recherche__img"  src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="16" height="16">
