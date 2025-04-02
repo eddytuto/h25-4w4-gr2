a4d369cef21e1d177fcc489b2e812f98fea5d7e7
Martin <emartin@cmaisonneuve.qc.ca>
2025-04-02 16:11:25 -0400

    s10c1 début de rest-api

--- single-post.php
+++ single-post.php
@@ -2,6 +2,7 @@
     <h1>-------- single-post.php ----------</h1>
     <section class="populaire">
         <div class="global">
+
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article>
             <?php
@@ -10,6 +11,10 @@
             ?>  
                 <h2><?php the_title(); ?></h2>
                 <div><?php the_content() ?>
+                <?php the_category(); ?>
+                <?php  $tableau = get_the_category(); 
+                // print_r ($tableau);
+                ?>
                 <p>Température maximum: <?php the_field('temperature_maximum') ?>&#176;C</p>
                 <p>Température minimum: <?php the_field('temperature_minimum') ?>&#176;C</p>
                 <p>Température moyenne: <?php the_field('temperature_moyenne') ?>&#176;C</p>
