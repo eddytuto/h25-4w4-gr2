674fe8e9fa7a85e652e2c0e93be3bbbe6839bcb8
Martin <emartin@cmaisonneuve.qc.ca>
2025-04-30 15:59:14 -0400

    s13c2 elments animé change pour chacun des slides

--- gabarits/hero.php
+++ gabarits/hero.php
@@ -14,11 +14,21 @@
             <input  class="hero__radio__input" data-id_radio="2" type="radio" name="carroussel">
         </div>
         <div class="hero__contenu global">
+            <div class="hero__animation hero__animation--active">
+                <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
+                <p class="hero__description"><?php bloginfo('description'); ?></p>
+            </div>
+            <div class="hero__animation ">
+                <h1 class="hero__titre">Lorem ipsum dolor</h1>
+                <p class="hero__description">Consectetur adipisicing elit. Magni voluptatem quas nobis, inventore sit odio debitis </p>
+            </div>
+            <div class="hero__animation ">
+                <h1 class="hero__titre">aaaa aaaa aaaaa aaaaaa</h1>
+                <p class="hero__description">zzzzzzz zzzzzz zzzzzz zzzzzzz zzzzzz zzzzzzz  </p>
+            </div>
 
-            <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
-            <p class="hero__description"><?php bloginfo('description'); ?>
+   
 
-            </p>
             <p class="hero__courriel">
             <?php bloginfo('admin_email'); ?>
             </p>
--- js/carrousel.js
+++ js/carrousel.js
@@ -2,19 +2,28 @@
 console.log("carrousel.js")
 let hero__radio__input = document.querySelectorAll(".hero__radio__input")
 let hero__carrousel = document.querySelectorAll(".hero__carrousel")
+let hero__animation = document.querySelectorAll(".hero__animation")
 console.log(hero__carrousel.length)
 console.log("hero__radio__input = " , hero__radio__input.length)
 hero__radio__input.forEach((radio) =>{
     radio.addEventListener('mousedown',function(){
         parcourir_carrousel()
+        parcourir_animation()
         console.log(radio.dataset.id_radio) 
         hero__carrousel[radio.dataset.id_radio].classList.add("hero__carrousel--active")
+        hero__animation[radio.dataset.id_radio].classList.add("hero__animation--active")
     })
 })
 
 function parcourir_carrousel(){
     hero__carrousel.forEach(element => {
         element.classList.remove("hero__carrousel--active")
+    });
+}
+
+function parcourir_animation(){
+    hero__animation.forEach(element => {
+        element.classList.remove("hero__animation--active")
     });
 }
 
