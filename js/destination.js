347e5051c5fe2cca744097a0de916fdfca69d131
Martin <emartin@cmaisonneuve.qc.ca>
2025-05-07 17:48:04 -0400

    s13 <base home_url( '/' ) pour définir l'adresse de la page d'accueil utile pour rest-api

--- js/destination.js
+++ js/destination.js
@@ -4,9 +4,9 @@
 (function(){
     console.log("destination.js")
     let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
-  
+    const domaine = document.querySelector('base').href;
     /* la technique utilisée pour extraire l'url doit êtere généralisée */
-    const domaine = window.origin + "/4w4-gr2/"
+    //const domaine = window.origin + "/4w4-gr2/"
 
     parcourir_bouton()
     mon_fetch(categoryId)
