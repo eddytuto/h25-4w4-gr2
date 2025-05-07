bd54bfa316e8d4e96704292ca5551647c7303445
Martin <emartin@cmaisonneuve.qc.ca>
2025-05-07 16:29:08 -0400

    s14 template de page template-evenement et rest-api amélioré

--- js/destination.js
+++ js/destination.js
@@ -3,11 +3,13 @@
  */
 (function(){
     console.log("destination.js")
-    const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
-    const domaine = window.location.href
-    const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
-    console.log(apiUrl)
+    let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+  
+    /* la technique utilisée pour extraire l'url doit êtere généralisée */
+    const domaine = window.origin + "/4w4-gr2/"
+
     parcourir_bouton()
+    mon_fetch(categoryId)
 
 function parcourir_bouton(){
     const categorie__ul__li = document.querySelectorAll(".categorie__ul__li")
@@ -16,16 +18,20 @@ function parcourir_bouton(){
         elm.addEventListener('mousedown', function(){
             console.log(elm.tagName)
             console.log("elm.dataset.category_id = " , elm.dataset.category_id)
+            mon_fetch(elm.dataset.category_id)
         })
     })
 
 }
 
-
+function mon_fetch(id_category)
+{
+    apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${id_category}`;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             const destinationList = document.querySelector('.destination__list');
+            destinationList.innerHTML = ""
             data.forEach(article => {
                 const articleElement = document.createElement('div');
                 console.log(article.title.rendered)
@@ -39,4 +45,5 @@ function parcourir_bouton(){
             });
         })
         .catch(error => console.error('Erreur lors de la récupération des articles:', error));
-    })()
+    }  
+})()
